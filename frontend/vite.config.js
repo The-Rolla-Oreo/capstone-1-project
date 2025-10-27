807f54f8c4c500109402bfc95a2a7f5321a83d2f
Manav Raja Vinotha <manav-the-dev@protonmail.com>
2025-10-27 12:21:50 -0500

    C1P-2 Installed tailscale package and added as plugin.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),
-      VitePWA({ registerType: 'autoUpdate' })
+      VitePWA({ registerType: 'autoUpdate' }),
+      tailwindcss(),
   ],
     server: {
       proxy: {
