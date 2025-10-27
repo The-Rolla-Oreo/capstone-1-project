8feffe850314dec670288963eb38fb0014a3a482
Manav Raja Vinotha <manav-the-dev@protonmail.com>
2025-10-27 12:18:00 -0500

    C1P-2 Added vite-pwa plugin

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+      VitePWA({ registerType: 'autoUpdate' })
+  ],
     server: {
       proxy: {
           "/backend": {
