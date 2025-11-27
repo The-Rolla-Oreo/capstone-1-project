d1a41065c44184f50262c1eed0ead1e2c30b00ee
Manav Raja Vinotha <manav-the-dev@protonmail.com>
2025-11-27 17:42:07 -0600

    C1P-41 Installed dependencies and set up settings for s3

--- backend/settings.py
+++ backend/settings.py
@@ -16,6 +16,11 @@ class Settings(BaseSettings):
     USERS_COLLECTION: str
     PASSWORD_RESET_COLLECTION: str
 
+    # S3 Stuff
+    S3_ENDPOINT: str
+    S3_ACCESS_KEY: str
+    S3_SECRET_KEY: str
+
     # Email stuff
     SMTP_HOST: str
     SMTP_PORT: int
